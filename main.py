8ec40f90a280a75b0e17ef3c3131f85cb0fe9586
Tan Nguyen <tannguyen7070@gmail.com>
2022-10-01 18:37:57 +0000

    Finished download.py

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-# External 
+# External
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 # Internal
 from src import download
 
-
 if __name__ == "__main__":
-  nltk.download([
-    "names",
-    "stopwords",
-    "state_union",
-    "twitter_samples",
-    "movie_reviews",
-   "averaged_perceptron_tagger",
-    "vader_lexicon",
-    "punkt" ], quiet=True)
-
   # Get Data from CSV
+  print("Downloading...")
   pos, neg = download.get_data()
-
+  print("Finished downloading...")
 
   # Testing
   sia = SentimentIntensityAnalyzer()
-  for ind in range(5):
-    print(pos[ind][0] + "\n\n")
+  for ind in range(2):
+    print(pos[ind][0])
     #print(sia.polarity_scores(dataset[ind][0]))
-  
--- src/download.py
+++ src/download.py
@@ -24,24 +24,23 @@ def cleaner(text):
   return text
 
 
-# Clean Text Data with NLTK
-def nltk_cleaner(text):
-  # Get list of unneeded english words
-  unwanted = nltk.corpus.stopwords.words("english")
-  unwanted.extend([w.lower() for w in nltk.corpus.names.words()])
-
+# Convert Text to List of Important Words
+def nltk_cleaner(text, remove):
+  # Remove unneeded words
   words = nltk.word_tokenize(text)
-
-  
-    
-  
-  return text
+  words = [word for word in words if word not in remove]
+  return words
 
 
 # Get and Format Data from CSV Dataset
 def get_data():
   pos_data = []
   neg_data = []
+
+  # List of unneeded english words
+  unwanted = nltk.corpus.stopwords.words("english")
+  unwanted.extend([w.lower() for w in nltk.corpus.names.words()])
+
   with open('data/kaggle_parsed_dataset.csv') as file:
     csv_reader = csv.reader(file,
                             quotechar='"',
@@ -53,18 +52,19 @@ def get_data():
       if row_count != 0:
         # Clean Text
         text = cleaner(row[3][1:-1])
-        text = nltk_cleaner(text)
+
+        # Convert to list of important words
+        words = nltk_cleaner(text, unwanted)
 
         # Output Data as a Tuple including Text, Flag
-        data_tup = (text, int(row[1]))
+        data_tup = (words, int(row[1]))
 
         # Put in corresponding list
         if int(row[1]) == 0:
           pos_data.append(data_tup)
         else:
           neg_data.append(data_tup)
-          
-          
+
       row_count += 1
 
   return pos_data, neg_data
